702641fe0a6231e01eaab23a6561dd32531db3f6
fachrulprasetyo <fachrulprasetyo98@gmail.com>
2024-02-02 20:47:37 +0700

    add function regist

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 5000,
-  viewportHeight: 650,
-  viewportWidth: 990,
-
   e2e: {
-    // baseUrl: "https://magento.softwaretestingboard.com/",
+    baseUrl: "https://magento.softwaretestingboard.com/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
 
-    defaultCommandTimeout: 10000,
+    defaultCommandTimeout: 5432,
+    viewportWidth: 990,
+    viewportHeight: 650,
   },
 });
--- cypress/e2e/web-magento/regist.cy.js
+++ cypress/e2e/web-magento/regist.cy.js
@@ -1,12 +1,34 @@
 describe("Regist Function", () => {
-  it("Verify Success Registration", () => {
-    cy.visit("https://magento.softwaretestingboard.com/");
+  beforeEach(() => {
+    cy.visit("");
+  });
+
+  it.only("Verify Success Registration", () => {
+    cy.contains("Create an Account").click();
+    cy.get("#firstname").clear().type("Sanbercode");
+    cy.get("#lastname").clear().type("Academy");
+    cy.get("#email_address").clear().type("sanbercode123@gmail.com");
+    cy.get("#password").clear().type("Sanber123");
+    cy.get("#password-confirmation").clear().type("Sanber123");
+    cy.get(".action.submit.primary").click();
+    cy.get(".message-success").should(
+      "contain.text",
+      "Thank you for registering with Main Website Store."
+    );
+  });
+
+  // Failed Regist [email has already been used]
+  it("Verify Failed Registration", () => {
     cy.contains("Create an Account").click();
     cy.get("#firstname").clear().type("Sanber");
     cy.get("#lastname").clear().type("Code");
     cy.get("#email_address").clear().type("sanbercode@gmail.com");
-    cy.get("#password").clear().type("sanber123");
-    cy.get("#password-confirmation").clear().type("sanber123");
+    cy.get("#password").clear().type("Sanber123");
+    cy.get("#password-confirmation").clear().type("Sanber123");
     cy.get(".action.submit.primary").click();
+    cy.get(".message-error").should(
+      "contain.text",
+      "There is already an account with this email address."
+    );
   });
 });
